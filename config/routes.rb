a07ea48062f19707c08b57dfbf2e1b9c3ca30dc8
na.luthfi <luthfi.aida@kumparan.com>
2021-01-23 11:11:58 +0700

    feature: adding picture uploads using carrierwave

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :ideas, only: %i(new index update show edit destroy)
+  resources :ideas
   root to: redirect('ideas')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
